383f8cd924ca5747d37949b0bcd468ccce6525a6
Dazhyar <dazhyarzanyar@gmail.com>
2024-10-08 21:01:55 +0300

    created constructors and get methods for the Node class

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Node client = new Node("localhost", "6000", true);
+
+        System.out.println(client.getClientIPAddr());
+        System.out.println(client.getClientPortNumber());
+
+        Node server = new Node("localhost", "7000", false);
+
+        System.out.println(server.getServerIPAddr());
+        System.out.println(server.getServerPortNumber());
     }
 }
--- Node.java
+++ Node.java
@@ -1,13 +1,58 @@
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 public class Node {
     private InetAddress clientIPAddr;
     private InetAddress serverIPAddr;
-    private String clientPortAddr;
-    private String serverPortAddr;
+    private String clientPortNumber;
+    private String serverPortNumber;
 
-    public Node(InetAddress clientIPAddr, String clientPortAddr) {
-        this.clientIPAddr = clientIPAddr;
-        this.clientPortAddr = clientPortAddr;
+    // Constructor for instances that act as either a client or a server
+    public Node(String ipAddr, String portNumber, boolean isClient) {
+        if (isClient) {
+            try {
+                this.clientIPAddr = InetAddress.getByName(ipAddr);
+                this.clientPortNumber = portNumber;
+            } catch (UnknownHostException e) {
+                System.out.println(e);
+            }
+        } else {
+            try {
+                this.serverIPAddr = InetAddress.getByName(ipAddr);
+                this.serverPortNumber = portNumber;
+            } catch (UnknownHostException e) {
+                System.out.println(e);
+            }
+        }
+    }
+
+    // Construtcor for instances that act as both clients and servers
+    public Node(String clientIPAddr, String clientPortNumber,
+    String serverIPAddr, String serverPortNumber) {
+        try {
+            this.clientIPAddr = InetAddress.getByName(clientIPAddr);
+            this.clientPortNumber = clientPortNumber;
+            
+            this.serverIPAddr = InetAddress.getByName(serverPortNumber);
+            this.serverPortNumber = serverPortNumber;
+        } catch (UnknownHostException e) {
+            System.out.println(e);
+        }
+    }
+
+    public String getClientIPAddr() {
+        return this.clientIPAddr.toString();
+    }
+
+    public String getServerIPAddr() {
+        return this.serverIPAddr.toString();
+    }
+
+    public String getClientPortNumber() {
+        return this.clientPortNumber;
+    }
+
+    public String getServerPortNumber() {
+        return this.serverPortNumber;
     }
 }
